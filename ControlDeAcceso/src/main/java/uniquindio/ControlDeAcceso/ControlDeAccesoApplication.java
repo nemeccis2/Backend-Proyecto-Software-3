af24149876a4c1dbec29c720eefd852319ac047d
nemeccis2 <83183453+nemeccis2@users.noreply.github.com>
2022-05-17 20:27:38 -0500

    V 0.0.1 Backend
    Creacion del proyecto con maven y spring y agregadas algunas de las librerias a utilizar

---
+++ ControlDeAcceso/src/main/java/uniquindio/ControlDeAcceso/ControlDeAccesoApplication.java
@@ -0,0 +1,13 @@
+package uniquindio.ControlDeAcceso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlDeAccesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlDeAccesoApplication.class, args);
+	}
+
+}
---
+++ ControlDeAcceso/src/test/java/uniquindio/ControlDeAcceso/ControlDeAccesoApplicationTests.java
@@ -0,0 +1,13 @@
+package uniquindio.ControlDeAcceso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlDeAccesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
