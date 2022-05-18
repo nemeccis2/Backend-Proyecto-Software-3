ec9e9ebb12d6eb9069192e2732ee14991bb6f912
nemeccis2 <83183453+nemeccis2@users.noreply.github.com>
2022-05-17 22:11:37 -0500

    V 0.0.5 Implementacion del modelo
    Implementacion de los atributos de las clases con la libreria Lombok para limpieza del codigo y javax.persistence para la base de datos

--- ControlDeAcceso/src/main/java/uniquindio/ControlDeAcceso/Model/Carrera.java
+++ ControlDeAcceso/src/main/java/uniquindio/ControlDeAcceso/Model/Carrera.java
@@ -1,4 +1,40 @@
 package uniquindio.ControlDeAcceso.Model;
 
-public class Carrera {
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.validator.constraints.Length;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Carrera implements Serializable {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer codigoCarrera;
+
+    @Column(name="nombre_carrera", length=40)
+    @Length(max = 40, min = 5, message = "Longitud invalida para el nombre de la carrera")
+    private String nombreCarrera;
+
+    @OneToMany(mappedBy="carrera")
+    @JsonIgnore
+    @ToString.Exclude
+    private List<Usuario> usuarios;
+
+    public void setCarrera(Carrera carrera){
+        this.codigoCarrera = carrera.getCodigoCarrera();
+        this.nombreCarrera = carrera.getNombreCarrera();
+    }
+
 }
--- ControlDeAcceso/src/main/java/uniquindio/ControlDeAcceso/Model/Rol.java
+++ ControlDeAcceso/src/main/java/uniquindio/ControlDeAcceso/Model/Rol.java
@@ -1,4 +1,40 @@
 package uniquindio.ControlDeAcceso.Model;
 
-public class Rol {
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.validator.constraints.Length;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+//@Table(name="rol")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Rol implements Serializable {
+    @Id
+    @Column(name="codigo_rol")
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer codigoRol;
+
+    @Column(name="nombre_rol", length=40)
+    @Length(max = 40, min = 5, message = "Longitud invalida para el nombre del rol")
+    private String nombreRol;
+
+    @OneToMany(mappedBy = "rol")
+    @ToString.Exclude
+    @JsonIgnore
+    private List<Usuario> usuarios;
+
+    public void setRol(Rol rol){
+        this.codigoRol = rol.getCodigoRol();
+        this.nombreRol = rol.getNombreRol();
+    }
 }
